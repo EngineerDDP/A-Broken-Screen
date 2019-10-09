7e1a6ca4f9d855f83443314c6a86a5b6a6db43f3
Engineer_DDP <zzaddp@live.com>
2019-10-09 20:42:00 +0800

    添加项目文件。

---
+++ 电视坏了/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace 电视坏了
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.栗晨晖 = new System.Windows.Forms.Timer(this.components);
+            this.PicBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // 栗晨晖
+            // 
+            this.栗晨晖.Enabled = true;
+            this.栗晨晖.Interval = 35;
+            this.栗晨晖.Tick += new System.EventHandler(this.lch_Tick);
+            // 
+            // PicBox
+            // 
+            this.PicBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PicBox.Location = new System.Drawing.Point(0, 0);
+            this.PicBox.Name = "PicBox";
+            this.PicBox.Size = new System.Drawing.Size(1904, 1041);
+            this.PicBox.TabIndex = 0;
+            this.PicBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.PicBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer 栗晨晖;
+        private System.Windows.Forms.PictureBox PicBox;
+    }
+}
+
---
+++ 电视坏了/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 电视坏了
+{
+    public partial class Form1 : Form
+    {
+        private const Int32 WIDTH = 1920;
+        private const Int32 SIZE = 7;
+        private const Int32 HEIGHT = 1080;
+        Bitmap map;
+
+        private int i = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            map = new Bitmap(WIDTH, HEIGHT);
+        }
+
+        private void flash()
+        {
+            Graphics g = Graphics.FromImage(map);
+            //获取填充色画刷
+            SolidBrush b = new SolidBrush(System.Drawing.Color.Black);
+            //创建可用矩阵
+            Rectangle rect = new Rectangle(0, 0, SIZE, SIZE);
+            int[,] ab = new int[HEIGHT / SIZE, WIDTH / SIZE];
+            Random r = new Random();
+            int i, j;
+            for (i = 0; i < HEIGHT / SIZE; ++i)
+            {
+                for (j = 0; j < WIDTH / SIZE; ++j)
+                {
+                    ab[i, j] = r.Next() % 2 == 0 ? 0 : 1;
+                }
+            }
+            for (i = 0; i < HEIGHT / SIZE; ++i)
+            {
+                for (j = 0; j < WIDTH / SIZE; ++j)
+                {
+                    if (ab[i, j] == 1)
+                    {
+                        rect.Location = new Point(j * SIZE, i * SIZE);
+                        g.FillRectangle(b, rect);
+                    }
+                }
+            }
+            if (PicBox.Image != null)
+                PicBox.Image.Dispose();
+            PicBox.Image = map;
+            map = new Bitmap(WIDTH, HEIGHT);
+            g.Dispose();
+            b.Dispose();
+        }
+
+        private void changebgc()
+        {
+            switch(i)
+            {
+                case 0:
+                    PicBox.BackColor = Color.Blue;
+                    break;
+                case 1:
+                    PicBox.BackColor = Color.Red;
+                    break;
+                case 2:
+                    PicBox.BackColor = Color.Green;
+                    break;
+                case 3:
+                    PicBox.BackColor = Color.Purple;
+                    break;
+                case 4:
+                    PicBox.BackColor = Color.Yellow;
+                    i = -1;
+                    break;
+                default:
+                    i = -1;
+                    break;
+            }
+            ++i;
+        }
+
+        private void lch_Tick(object sender, EventArgs e)
+        {
+            flash();
+        }
+    }
+}
